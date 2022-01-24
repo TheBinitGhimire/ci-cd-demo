ed3873799a75d26396bdd75d7adc8462d14cc8f9
Binit Ghimire <thebinitghimire@gmail.com>
2022-01-25 03:07:03 +0545

    Added the application source codes!

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#[macro_use] extern crate rocket;
+use rocket::{build, response::content::Html};
+
+#[get("/")]
+fn index() -> Html<&'static str> {
+    Html("<h1 align=\"center\">\
+        Hello, there!\
+    </h1>\
+    <h2 align=\"center\">\
+        This is the Initial Deployment.\
+    </h2>")
+}
+
+#[launch]
+fn rocket() -> _ {
+    build().mount("/", routes![index])
+}
